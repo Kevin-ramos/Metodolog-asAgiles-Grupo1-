531f896409a415e1b2cd86c4d79c3dfb4554acb9
Marco Salazar <andressalazar714@gmail.com>
2021-11-19 23:44:54 -0500

    Revert "Revert "Merge branch 'Desarrollo' of https://github.com/Kevin-ramos/Metodolog-asAgiles-Grupo1- into Desarrollo""
    This reverts commit 7a0f4e12451428d55efd63518ec53064bf0bbef6.

--- src/Producto.java
+++ src/Producto.java
@@ -36,13 +36,33 @@ public void setCantidad(int cantidad) {
 	this.cantidad = cantidad;
 }
 
-public static Producto registrar(String codigo, String nombre, int cantidad) {
-	this.codigo=codigo;
-	this.nombre=nombre;
-	this.cantidad=cantidad;
-	return this;
+
+public static void registrar(String codigo, String nombre, int cantidad, Inventario inventario) {
+	Producto p = new Producto(codigo,nombre, cantidad);
+	int indiceDelProductoEnInventario =inventario.verificarProducto(p)
+	if( indiceDelProductoEnInventario == -1) {
+		inventario.agregarProducto(p);
+		System.out.println("Producto ingresado con éxito.");
+	}else {
+		System.out.println("El producto ya existe.");
+	}
 }
 
+@Override
+public String toString() {
+	return "Código= " + codigo + ", Nombre= " + nombre + ", Cantidad= " + cantidad + ".";
+}
 
+	@Override
+	public int hashCode() {
+		return nombre.hashCode();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+	Producto p=(Producto)obj;
+		return p.getCodigo().equals(this.getCodigo());
+
+	}
 }
 
--- src/Test.java
+++ src/Test.java
@@ -5,7 +5,86 @@ public class Test {
 		// TODO Auto-generated method stub
 		
 		
+
+		System.out.println("Bienvenido al Sistema de inventario para bodegas de alimentos (SIBA)");
 		
+		int opcion;
+		int opcion2;
+	    Scanner entrada = new Scanner(System.in);
+	    
+	    Producto.registrar("1", "Manzana",30, inventario);
+	    Producto.registrar("2", "Pera", 50, inventario);
+	    Producto.registrar("3", "Lata de Atún", 100, inventario);
+	    Producto.registrar("4", "Quintal de Arroz", 10, inventario);
+	    Producto.registrar("5", "Chocolate Galak", 33, inventario);
+	    
+		do{
+			
+            System.out.println("\nMENÚ DE OPCIONES \n"
+                                + "1.- Productos \n"
+                                + "2.- Salir.\n");
+            System.out.print("Ingrese el número de opción que desea: ");
+            
+            opcion = entrada.nextInt();
+            entrada.nextLine();
+            switch (opcion){
+                case 1:
+                    
+                	do{
+            			
+                        System.out.println("\n******************************Productos******************************  \n"
+                                            + "1.- Registrar \n"
+                                            + "2.- Consultar \n"
+                                            + "3.- Eliminar \n"
+                                            + "4.- Regresar.\n");
+                        System.out.print("Ingrese el número de opción que desea: ");
+                        opcion2 = entrada.nextInt();
+                        entrada.nextLine();
+                        switch (opcion2){
+                            case 1:
+                            	String codigo;
+                            	String nombre;
+                            	int cantidad;
+                            	System.out.print("Ingrese el codigo del nuevo producto: ");
+                            	codigo = entrada.nextLine();
+                            	System.out.print("Ingrese el nombre del nuevo producto: ");
+                            	nombre = entrada.nextLine();
+                            	System.out.print("Ingrese el cantidad del nuevo producto: ");
+                            	cantidad = entrada.nextInt();
+                            	entrada.nextLine();
+                            	Producto.registrar(codigo, nombre, cantidad, inventario);
+                                break;
+                            case 2:
+                            	System.out.print("Ingrese el código del producto a consultar: ");
+                            	codigo = entrada.nextLine();
+                            	Producto producto = inventario.consultarProducto(codigo);
+                            	if (producto == null) {
+                            		System.out.println("Producto no encontrado.");
+                            	}else
+                            		System.out.println(producto);
+                                break;
+                            case 3:
+                            	System.out.print("Ingrese el código del producto a eliminar: ");
+                            	codigo = entrada.nextLine();
+                                inventario.eliminarProducto(codigo);
+                                break;
+                            case 4:
+                                System.out.println("\nSaliendo de Producto.\n");
+                                break;
+                            default:
+                                System.out.println("ERROR: La opción ingresada no esta permitida ingrese otra.");
+                                break;
+                        }
+                	}while (opcion2!=4);
+                    break;
+                case 2:
+                    System.out.println("Saliendo... Muchas Gracias.");
+                    break;
+                default:
+                    System.out.println("ERROR: La opción ingresada no esta permitida ingrese otra.");
+                    break;
+            }
+    }while (opcion!=2);
 
 	}
 
